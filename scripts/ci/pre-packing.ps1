d7adf4b0a101d29a0bf59dabfd55ec739c30f0b5
無常 <3142324836@qq.com>
2024-08-14 00:26:10 +0800

    Add build workflow

---
+++ scripts/ci/pre-packing.ps1
@@ -0,0 +1,21 @@
+﻿param (
+    [string]$DirectoryPath
+)
+
+# Check if the directory path is provided and exists
+if (-not (Test-Path -Path $DirectoryPath)) {
+    exit
+}
+
+$LOG_PATH = $DirectoryPath + "/debug"
+if ((Test-Path $LOG_PATH) -eq "True") {
+	Remove-Item $LOG_PATH -Recurse
+}
+
+$REMOVE_PATH = $DirectoryPath + "/*"
+Remove-Item $REMOVE_PATH -Include "*.iobj"
+Remove-Item $REMOVE_PATH -Include "*.ipdb"
+Remove-Item $REMOVE_PATH -Include "*.exp"
+Remove-Item $REMOVE_PATH -Include "*.ilk"
+Remove-Item $REMOVE_PATH -Include "*.lib"
+Remove-Item $REMOVE_PATH -Include "*.a"
