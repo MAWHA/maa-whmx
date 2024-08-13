558759a0cb733faab7bdafec86b5517237e79254
無常 <3142324836@qq.com>
2024-08-13 20:10:30 +0800

    Removed BOM

--- whmx-assistant/src/Main.cpp
+++ whmx-assistant/src/Main.cpp
@@ -1,4 +1,4 @@
-﻿/* Copyright 2024 周上行Ryer
+/* Copyright 2024 周上行Ryer
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
