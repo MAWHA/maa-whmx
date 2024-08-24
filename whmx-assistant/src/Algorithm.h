c8729964bcb0858c7246181ef4322ef470aecf88
周上行Ryer <melaiiryer@gmail.com>
2024-08-24 08:41:40 +0800

    feat: add convenient find method

--- whmx-assistant/src/Algorithm.h
+++ whmx-assistant/src/Algorithm.h
@@ -17,6 +17,8 @@
 
 #include <QtCore/QString>
 #include <QtCore/QList>
+#include <algorithm>
+#include <optional>
 
 int           min_edit_distance(const QString &src, const QString &dst);
 QList<double> softmax(const QList<double> &vec);
@@ -24,3 +26,36 @@ int           choice(const QList<double> &weights);
 int           choice(int min_index, int max_index);
 QList<int>    multi_choice(int n, int min_index, int max_index);
 double        eval_color_distance(const int (&lhs_rgb)[3], const int (&rhs_rgb)[3]);
+
+template <typename Container, typename T>
+    requires std::convertible_to<T, decltype(*std::declval<Container>().begin())>
+auto find_element(const Container &container, const T &value) {
+    return std::find(container.begin(), container.end(), value);
+}
+
+template <typename Container, typename T, typename G = std::remove_cvref_t<decltype(*std::declval<Container>().begin())>>
+    requires std::is_invocable_r_v<bool, T, const G &>
+auto find_element(const Container &container, T &&pred) {
+    return std::find_if(container.begin(), container.end(), pred);
+}
+
+template <
+    typename Container,
+    typename T,
+    typename E     = decltype(*std::declval<Container>().begin()),
+    typename MemFn = T (E::*)()>
+auto find_element(Container &&container, MemFn member, const T &value) {
+    return std::find_if(container.begin(), container.end(), [=](const auto &elem) {
+        return elem.*member == value;
+    });
+}
+
+template <typename Container, typename... Args, typename T = std::remove_cvref_t<decltype(*std::declval<Container>().begin())>>
+std::optional<T> find_element_opt(Container &&container, Args &&...args) {
+    const auto it = find_element(container, std::forward<Args>(args)...);
+    if (it == container.end()) {
+        return std::nullopt;
+    } else {
+        return std::make_optional(*it);
+    }
+}
